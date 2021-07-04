6bf9dbd2cd5a1463fda35c0c597184dcbd682c27
bonino97 <juanbonino97@gmail.com>
2021-07-04 18:58:52 -0300

    Finishing first version of working game

--- damas-v2/js/game.js
+++ damas-v2/js/game.js
@@ -59,9 +59,9 @@ function drawBoard() {
 
   moveCountElem.innerHTML = moveCount;
 
-  //   if (gameInProgress && isTheGameOver()) {
-  //     endGame();
-  //   }
+  if (gameInProgress && isTheGameOver()) {
+    endGame();
+  }
 }
 
 function drawPiece(p, color, selected) {
@@ -132,6 +132,117 @@ function getCursorPosition(e) {
   let cell = new Box(Math.floor(y / pieceHeight), Math.floor(x / pieceWidth));
   return cell;
 }
+
+function getLegalMoves() {
+  let theLegalMoves = [];
+  let z = 0;
+
+  while (z < pieces.length) {
+    if (
+      (whiteTurn && whitePieceColor === pieces[z].color) ||
+      (redTurn && redPieceColor === pieces[z].color)
+    ) {
+      let newMovements = getLegalMovesPerPiece(pieces[z]);
+
+      let t = 0;
+      while (t < newMovements.length) {
+        if (newMovements[t] instanceof Jump) {
+          let oneJump = newMovements.splice(t, 1);
+          theLegalMoves = oneJump.concat(theLegalMoves);
+        } else {
+          t++;
+        }
+      }
+
+      theLegalMoves = theLegalMoves.concat(newMovements);
+    }
+    z++;
+  }
+  return theLegalMoves;
+}
+
+function getLegalMovesPerPiece(onePiece) {
+  let i = -1;
+  let row = 0;
+  let column = 0;
+  let someLegalMoves = [];
+  let empty = false;
+
+  while (i < 2) {
+    if ((onePiece.row != 0 && whiteTurn) || (onePiece.row != 7 && redTurn)) {
+      if (
+        (onePiece.column != 0 && i === -1) ||
+        (onePiece.column != 7 && i === 1)
+      ) {
+        if (whiteTurn) {
+          row = onePiece.row - 1;
+          column = onePiece.column + i;
+        } else {
+          row = onePiece.row + 1;
+          column = onePiece.column + i;
+        }
+        let j = 0;
+        let existe = false;
+        while (j < pieces.length && existe === false) {
+          if (pieces[j].row === row && pieces[j].column === column) {
+            existe = true;
+            if (pieces[j].color != onePiece.color) {
+              if (
+                i < 0 &&
+                whiteTurn &&
+                onePiece.column >= 2 &&
+                onePiece.row >= 2
+              ) {
+                row = onePiece.row - 2;
+                column = onePiece.column - 2;
+                empty = emptyBox(row, column);
+              } else if (
+                i > 0 &&
+                whiteTurn &&
+                onePiece.column <= 5 &&
+                onePiece.row >= 2
+              ) {
+                row = onePiece.row - 2;
+                column = onePiece.column + 2;
+                empty = emptyBox(row, column);
+              } else if (
+                i < 0 &&
+                redTurn &&
+                onePiece.column >= 2 &&
+                onePiece.row <= 5
+              ) {
+                row = onePiece.row + 2;
+                column = onePiece.column - 2;
+                empty = emptyBox(row, column);
+              } else if (
+                i > 0 &&
+                redTurn &&
+                onePiece.column <= 5 &&
+                onePiece.row <= 5
+              ) {
+                row = onePiece.row + 2;
+                column = onePiece.column + 2;
+                empty = emptyBox(row, column);
+              }
+            }
+          } else {
+            j++;
+          }
+        }
+        if (existe === false) {
+          let aMove = new Move(onePiece.row, onePiece.column, row, column);
+          someLegalMoves.push(aMove);
+        } else if (existe === true && empty === true) {
+          let aJump = new Jump(onePiece.row, onePiece.column, row, column);
+          someLegalMoves.unshift(aJump);
+        }
+      }
+    }
+    i = i + 2;
+  }
+  return someLegalMoves;
+}
+
 function clickManager(e) {
   let row = getCursorPosition(e);
   for (let i = 0; i < numPieces; i++) {
@@ -142,6 +253,96 @@ function clickManager(e) {
   }
   clickOnEmptyCell(row);
 }
+
+function clickOnPiece(pieceIndex) {
+  if (
+    (whiteTurn && pieces[pieceIndex].color === whitePieceColor) ||
+    (redTurn && pieces[pieceIndex].color === redPieceColor)
+  ) {
+    if (selectedPieceIndex === pieceIndex) {
+      return;
+    }
+    selectedPieceIndex = pieceIndex;
+    selectedPieceHasMoved = false;
+    drawBoard();
+  } else {
+    alert('No es tu turno');
+  }
+}
+
+function clickOnEmptyCell(cell) {
+  if (selectedPieceIndex === -1) {
+    return;
+  }
+
+  let direction = 1;
+  if (pieces[selectedPieceIndex].color === whitePieceColor) direction = -1;
+
+  let rowDiff = direction * (cell.row - pieces[selectedPieceIndex].row);
+  let columnDiff =
+    direction * (cell.column - pieces[selectedPieceIndex].column);
+  if (
+    rowDiff === 1 &&
+    Math.abs(columnDiff) === 1 &&
+    !(legalMoves[0] instanceof Jump)
+  ) {
+    showMovement(pieces[selectedPieceIndex], cell, false);
+
+    pieces[selectedPieceIndex].row = cell.row;
+    pieces[selectedPieceIndex].column = cell.column;
+
+    checkWinner();
+
+    changeTurn();
+    moveCount += 1;
+    selectedPieceIndex = -1;
+    selectedPieceHasMoved = false;
+    drawBoard();
+    numMoves += 1;
+    checkTie();
+    return;
+  } else if (
+    rowDiff === 1 &&
+    Math.abs(columnDiff) === 1 &&
+    legalMoves[0] instanceof Jump
+  ) {
+    alert('Hay saltos disponibles');
+  } else if (
+    Math.abs(rowDiff) === 2 &&
+    Math.abs(columnDiff) === 2 &&
+    isThereAPieceBetween(pieces[selectedPieceIndex], cell) &&
+    legalMoves[0] instanceof Jump
+  ) {
+    if (!selectedPieceHasMoved) {
+      moveCount += 1;
+    }
+
+    showMovement(pieces[selectedPieceIndex], cell, true);
+
+    pieces[selectedPieceIndex].row = cell.row;
+    pieces[selectedPieceIndex].column = cell.column;
+
+    if (indexToDelete > selectedPieceIndex) {
+      removePiece();
+      checkWinner();
+    } else {
+      checkWinner();
+      removePiece();
+    }
+
+    selectedPieceIndex = -1;
+    selectedPieceHasMoved = false;
+
+    numMoves = 0;
+    changeTurn();
+    drawBoard();
+    return;
+  }
+  selectedPieceIndex = -1;
+  selectedPieceHasMoved = false;
+  drawBoard();
+}
+
 function showMovement(box1, box2, jump) {
   console.log(box1, box2, jump);
   let movement = document.createElement('p');
@@ -153,14 +354,126 @@ function showMovement(box1, box2, jump) {
     document.getElementById('isTurn').innerHTML = 'White Turn';
   }
 }
+
+function checkWinner() {
+  if (
+    (whiteTurn &&
+      pieces[selectedPieceIndex].color === whitePieceColor &&
+      pieces[selectedPieceIndex].row === 0) ||
+    (redTurn &&
+      pieces[selectedPieceIndex].color === redPieceColor &&
+      pieces[selectedPieceIndex].row === 7)
+  ) {
+    let winnerPiece = pieces.splice(selectedPieceIndex, 1);
+    winner(winnerPiece[0]);
+  }
+}
+
+function changeTurn() {
+  if (whiteTurn) {
+    whiteTurn = false;
+    redTurn = true;
+  } else {
+    whiteTurn = true;
+    redTurn = false;
+  }
+}
+
+function isThereAPieceBetween(box1, box2) {
+  let existe = false;
+  let i = 0;
+  let row = 0;
+  let column = 0;
+
+  if (
+    whiteTurn &&
+    box2.column - box1.column === -2 &&
+    box2.row - box1.row === -2
+  ) {
+    column = box1.column - 1;
+    row = box1.row - 1;
+  } else if (
+    whiteTurn &&
+    box2.column - box1.column === 2 &&
+    box2.row - box1.row === -2
+  ) {
+    column = box1.column + 1;
+    row = box1.row - 1;
+  } else if (
+    redTurn &&
+    box2.column - box1.column === -2 &&
+    box2.row - box1.row === 2
+  ) {
+    column = box1.column - 1;
+    row = box1.row + 1;
+  } else if (
+    redTurn &&
+    box2.column - box1.column === 2 &&
+    box2.row - box1.row === 2
+  ) {
+    column = box1.column + 1;
+    row = box1.row + 1;
+  }
+  while (i < pieces.length && existe === false) {
+    if (pieces[i].row === row && pieces[i].column === column) {
+      if (box1.color !== pieces[i].color) {
+        existe = true;
+        indexToDelete = i;
+      } else {
+        alert('No puedes comer fichas de tu mismo color');
+      }
+    }
+    i++;
+  }
+  return existe;
+}
+
+function removePiece() {
+  pieces.splice(indexToDelete, 1);
+  indexToDelete = -1;
+  numPieces--;
+}
+
+function checkTie() {
+  if (numMoves >= 50 || tieAgreed) {
+    isTie = true;
+    endGame();
+  }
+}
+
+function isTheGameOver() {
+  legalMoves = getLegalMoves();
+  if (legalMoves.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function emptyBox(row, column) {
+  let y = 0;
+  let empty = true;
+  while (y < pieces.length && empty === true) {
+    if (pieces[y].row === row && pieces[y].column === column) {
+      empty = false;
+    } else {
+      y++;
+    }
+  }
+  return empty;
+}
+
+function winner(piece) {
+  pieces.push(new Queen(piece.row, piece.column, piece.color));
+}
+
 function newGame() {
-  // Reiniciamos variables.
   numMoves = 0;
   numPieces = 24;
   whiteTurn = true;
   redTurn = false;
 
-  pieces = []; // Vaciamos la lista de pieces, por si estamos pulsando el resetButton.
+  pieces = [];
 
   for (let i = 0; i < initialRows; i++) {
     for (let j = (i + 1) % 2; j < boardHeight; j = j + 2) {
@@ -187,6 +500,18 @@ function newGame() {
   gameInProgress = true;
 }
 
+function endGame() {
+  gameInProgress = false;
+  if (isTie) {
+    alert('Game over. Empate');
+  } else if (whiteTurn) {
+    alert('Game over. Ganan Rojas');
+  } else {
+    alert('Game over. Ganan Blancas');
+  }
+  newGame();
+}
+
 function playGame(canvaElement, moveCountElement) {
   canvasElement = canvaElement;
   canvasElement.width = pixelWidth;
@@ -195,6 +520,9 @@ function playGame(canvaElement, moveCountElement) {
   moveCountElem = moveCountElement;
   drawingContext = canvasElement.getContext('2d');
 
+  saveButton = document.getElementById('resetButton');
+  saveButton.onclick = newGame;
+  
   newGame();
 }
 
@@ -207,3 +535,20 @@ function Box(row, column, color) {
 function Queen(row, column, color) {
   Box.apply(this, [row, column, color]);
 }
+
+Queen.prototype = new Queen();
+Queen.prototype.constructor = Queen;
+
+function Move(r1, c1, r2, c2) {
+  this.fromRow = r1;
+  this.fromCol = c1;
+  this.toRow = r2;
+  this.toCol = c2;
+}
+
+function Jump(r1, c1, r2, c2) {
+  Move.apply(this, [r1, c1, r2, c2]);
+}
+
+Jump.prototype = new Move();
+Jump.prototype.constructor = Move;
